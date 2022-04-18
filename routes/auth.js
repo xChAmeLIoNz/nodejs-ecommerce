2d126508e4d766016558e8ab87b29e350aa31808
xChAmeLIoNz <afogliiseppe@gmail.com>
2022-04-18 03:43:12 +0200

    added login | NEEDS FIXES

--- routes/auth.js
+++ routes/auth.js
@@ -25,9 +25,23 @@ router.post('/register', async (req,res) => {
 //LOGIN
 router.post('/login', async (req,res) => {
     try {
+        const user = await User.findOne({username: req.body.username});
+        if(!user) {
+            res.status(400).json('Wrong USER credentials!');
+        }
+        const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.SEC_PASS);
+        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);//debug: fino a qua la psw viene decriptata correttamente (issue: IF ?)
+        console.log(originalPassword);
+        if(originalPassword !== req.body.password) {
+            return res.status(400).json('Wrong PASSWORD credentials!');
+        }
+        //U2FsdGVkX1+fwDANJral6p79qxV1o607DTXmZKRvBuw=  
         
+        const { password, ...others} = user._doc;
+        return res.status(200).json(user);
+
     } catch (error) {
-        
+        res.status(500).json(error);                
     }
 })
 
