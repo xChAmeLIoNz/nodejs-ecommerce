84c387f1dec538535af30e305e87fdb15079a6d5
xChAmeLIoNz <afogliiseppe@gmail.com>
2022-04-21 15:56:03 +0200

    fixed cart and negative number quantity in products collection

--- routes/product.js
+++ routes/product.js
@@ -18,24 +18,28 @@ router.get('/productName/:name', async (req,res) => {
 
     if(count > quantità) {
         res.status(400).json("We don't have enough of that!");
+    }else if (count <= quantità) {
+        const updatedQuantity = (quantità - count);
+        const newCart = new Cart(
+            {
+                username: "user",
+                products: [
+                    {
+                        productName: selectedProduct.title,
+                        quantity: count
+                    }
+                ]                                
+            }
+        );
+        const savedCart = await newCart.save();
+        const updatedProduct = await selectedProduct.update({quantity: updatedQuantity}); 
+        //METHOD UPDATE IS DEPRECATED, USE updateOne or updateMany instead
+    
+        res.status(200).json("Item/s added to cart!");
     }
-    const updatedQuantity = (quantità - count);
+    
 
-    const newCart = new Cart(
-        {
-            username: "ciao",
-            products: [
-                {
-                    productName: selectedProduct.title,
-                    quantity: count
-                }
-            ]                                
-        }
-    );
-    const savedCart = await newCart.save();
-    const updatedProduct = await selectedProduct.update({quantity: updatedQuantity});
-
-    res.status(200).json("Item/s added to cart!");
+    
 })
 
 module.exports = router;
