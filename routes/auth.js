0e448e39279768a95ca5233404bc92b5288edd28
xChAmeLIoNz <afogliiseppe@gmail.com>
2022-04-27 00:34:38 +0200

    added error message when user tries to register an already used username

--- routes/auth.js
+++ routes/auth.js
@@ -15,9 +15,16 @@ router.post('/register', async (req,res) => {
     
     try {
         const savedUser = await newUser.save();
-        res.status(200).sendFile(path.join(__dirname, '../src/templates', '/index.html'));
+        res.status(200).sendFile(path.join(__dirname, '../html/templates', '/index.html'));
     } catch (err) {
+        //se l'username esiste già nel db, viene mandata una pagina di errore
+        const controllo = await User.findOne({username:req.body.username});
+        if (controllo.username == req.body.username) {
+            res.status(400).sendFile(path.join(__dirname, '../html/templates', '/errore.html'));
+            return;
+        } 
         res.status(500).json(err);
+         
     }
 
 });
@@ -39,7 +46,7 @@ router.post('/login', async (req,res) => {
         //se provo a stampare il json con l'oggetto user (senza pswd, quindi "...others") non funziona, output = {}
         //return res.status(200).json(...others); //2:13 AM FUNZIONA CAZZO
 
-        res.status(200).sendFile(path.join(__dirname, '../src/templates', '/merce.html'));
+        res.status(200).sendFile(path.join(__dirname, '../html/templates', '/merce.html'));
     } catch (error) {
         res.status(500).json(error);                
     }
